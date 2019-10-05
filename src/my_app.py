cfd6cf851b56228ff5ea602bb275a17c633e6387
DenDen047 <sh.mn.nat@gmail.com>
2019-10-06 07:55:25 +0800

    checked Config groups
    https://cli.dev/docs/tutorial/config_groups

--- src/my_app.py
+++ src/my_app.py
@@ -1,13 +1,8 @@
 import hydra
 
 
-@hydra.main(config_path='config.yaml', strict=False)
+@hydra.main(config_path='conf')
 def my_app(cfg):
-    driver = cfg.db.driver
-    user = cfg.db.user
-    password = cfg.db.password
-    cfg.db.port = 3306
-
     print(cfg.pretty())
 
 if __name__ == "__main__":
