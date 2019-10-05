c2f5f5292641f09f2dfde7b652b3a487bd2c101f
DenDen047 <sh.mn.nat@gmail.com>
2019-10-06 07:46:09 +0800

    checked to turn on/off of the strict mode

--- src/my_app.py
+++ src/my_app.py
@@ -1,8 +1,13 @@
 import hydra
 
 
-@hydra.main(config_path='config.yaml')
+@hydra.main(config_path='config.yaml', strict=False)
 def my_app(cfg):
+    driver = cfg.db.driver
+    user = cfg.db.user
+    password = cfg.db.password
+    cfg.db.port = 3306
+
     print(cfg.pretty())
 
 if __name__ == "__main__":
